128ea7deb103025b04fe86c09f14cf50e2b70a2e
KAUKAB ERUM <kaukaberum2006@gmail.com>
2026-01-11 19:16:59 +0530

    Update test_voice_query.py

--- test_voice_query.py
+++ test_voice_query.py
@@ -1,40 +1,6 @@
 
 # test_voice_query
 
-'''
-import asyncio
-import shutil
-import os
-
-from speech_to_text_free import transcribe_audio_local
-from text_to_speech_free import synthesize_speech
-from ollama_llm import call_mistral
-
-
-async def test_voice_pipeline():
-    audio_path = "temp_audio/test.wav"  # your input audio
-
-    if not os.path.exists(audio_path):
-        print("❌ Test audio not found")
-        return
-
-    print("🎤 STT: Transcribing...")
-    user_text = await transcribe_audio_local(audio_path)
-    print("User Text:", user_text)
-
-    print("🧠 LLM: Ollama responding...")
-    bot_text = call_mistral(user_text)
-    print("Bot Text:", bot_text)
-
-    print("🔊 TTS: Generating voice...")
-    audio_out = await synthesize_speech(bot_text)
-
-    print("✅ Voice response saved at:", audio_out)
-
-
-if __name__ == "__main__":
-    asyncio.run(test_voice_pipeline())
-'''
 import asyncio
 from ollama_llm import call_mistral
 from text_to_speech_free import synthesize_speech
@@ -52,3 +18,4 @@ async def main():
     print("✅ Voice saved at:", audio_path)
 
 asyncio.run(main())
+
